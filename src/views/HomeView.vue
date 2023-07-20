3826bb246b4791ff0917b0e4ddaad4e216b43a66
juliancruzsanchez <kickbutt.js@gmail.com>
2023-07-20 15:14:18 -0600

    Added Listing fetching on homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="home">
-]    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import ParseUtility from "../utilities/parse";
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      test: "Test",
+      listings: []
+    }
+  },
+  async mounted() {
+    const _listings = []
+    const listingObject = new Parse.Object('Listing');
+    const query = new Parse.Query(listingObject);
+    const listings = await ParseUtility.getListings();
+    listings.forEach(listing => {
+      query.get(listing.id).then((result) => {
+        _listings.push(result.toJSON())
+      }).catch((error) => {
+        console.error('Error fetching listings:', error);
+      });
+    });
+    this.$data.listings = _listings
+  },
   components: {
     HelloWorld
   }
